5dbf3893ab5e804f9b0bbfacbaac68ad03482d68
Mr.Smirnov.Andrey <Mr.Smirnov.Andrey@gmail.com>
2025-02-15 20:47:31 +0300

    [Project] Косметические доработки конфигураций. Добавлены кастомные метрики для сервиса Orders. Добавлен дескриптор Графана Дашборд-а.

--- Project/Ex_Order/src/main/java/ru/otus/project/orders/controller/OrderController.java
+++ Project/Ex_Order/src/main/java/ru/otus/project/orders/controller/OrderController.java
@@ -1,14 +1,13 @@
 package ru.otus.project.orders.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.project.orders.adapter.MqAdapter;
 import ru.otus.project.orders.domain.Order;
 import ru.otus.project.orders.service.OrderService;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -22,6 +21,8 @@ public class OrderController {
     private MqAdapter mqAdapter;
 
     @PostMapping("/buy")
+    @Counted(value = "createBuyOrder_counted")
+    @Timed(value = "createBuyOrder_timed", percentiles = {0.5, 0.95, 0.99})
     public Order createBuyOrder(@RequestBody Order order) {
         Order savedOrder = orderService.saveOrderToBuy(order);
         mqAdapter.sendOrderToBuyIntoTrade(savedOrder);
@@ -29,6 +30,8 @@ public class OrderController {
     }
 
     @PostMapping("/sell")
+    @Counted(value = "createSellOrder_counted")
+    @Timed(value = "createSellOrder_timed", percentiles = {0.5, 0.95, 0.99})
     public Order createSellOrder(@RequestBody Order order) {
         Order savedOrder = orderService.saveOrderToSell(order);
         mqAdapter.sendOrderToSellIntoTrade(savedOrder);
